28bb154b07c5b6365817100fd46cb3375531eb39
ncthang <thang_dth235765@student.agu.edu.vn>
2025-09-29 07:21:25 +0700

    Nộp BT

--- Chuong3_cau11.py
+++ Chuong3_cau11.py
@@ -1,11 +1,15 @@
 #Cau 11: Tính dãy số
-x=int(input("Nhập x:"))
-n=int(input("Nhập N:"))
-s=0
-for i in range(1,n+1):
- tu=x**i
- mau=1
- for j in range(1,i+1):
-     mau=mau*j
- s=s+(tu/mau)
-print("s({0},{1})={2}".format(x,n,s))
+while True:
+ n=int(input("Nhập 1 số nguyên dương"))
+ dem=0
+ for i in range(1,n+1):
+    if n % i ==0 :
+        dem+=1
+ if dem==2:
+    print(n,"Là số nguyên tố")
+ else:
+    print(n,"Không là số nguyên tố")
+ hoi=input("Tiếp không Thím?(c/k):")
+ if hoi is "k":
+    break
+print("BYE!")
